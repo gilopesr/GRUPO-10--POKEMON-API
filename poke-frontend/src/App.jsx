0201d0a70edd499412861d4d5244966fca14788c
Giovana Lopes <gilopes2005@gmail.com>
2025-10-28 09:53:04 -0300

    Add files via upload

---
+++ poke-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return(
+  <div class='card'>
+    <div class='card-info'>
+      <h1>nome Pokemon</h1>
+      <p>altura:</p>
+      <p>peso:</p>
+    </div>
+
+    <div class='card-tipagem'>
+      <h1>Tipo:</h1>
+      <p>água</p>
+      <h1>Fraqueza:</h1>
+      <p>fq 1</p>
+      <p>fq 2</p>
+    </div>
+  </div>
+  )
+}
+
+export default App
